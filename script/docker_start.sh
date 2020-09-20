13ee1b7b3c16de9945880ce1ddbb7e60d5e2a095
Vitah <linw1225@gmail.com>
2020-09-20 17:08:26 +0800

    Chore：docker启动脚本

--- script/docker_start.sh
+++ script/docker_start.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
-easy_path=/home/${USER}/easy_data
-if [ ! -d $easy_path ];then
-   mkdir $easy_path
-else
-   echo $easy_path
-fi
-docker run -it --shm-size="2g" --gpus=all -v ${easy_path}:/easy easy_ubuntu
 
+function main() {
+   USER_ID=$(id -u)
+   USER=$(whoami)
+   GRP=$(id -g -n)
+   GRP_ID=$(id --group)
+
+   echo "Starting docker container..."
+   echo "Current user id:" $USER_ID
+   echo "Current user name:" $USER
+   echo "Current group id:" $GRP_IDP
+   echo "Current group name:"$GRP
+
+   if [ ! -d "/home/$USER/easy" ]; then
+      echo "easy_path not exist, create dir /home/$USER/easy"
+      mkdir "/home/$USER/easy"
+   fi
+
+   docker run -it --shm-size=1g \
+      -e DOCKER_USER_ID=$USER_ID \
+      -e DOCKER_GRP="$GRP" \
+      -e DOCKER_GRP_ID=$GRP_ID \
+      -v /home/$USER/easy:/easy easy_ubuntu
+
+   docker run -it --shm-size="2g" -v ${easy_path}:/easy easy_ubuntu
+}
+
+main
