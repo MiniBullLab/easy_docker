c573c94e7f17a56e3f2062e4832b42610ac7853f
Vitah <linw1225@gmail.com>
2020-12-02 22:13:15 +0800

    Feat：docker启动脚本调整，指定启动容器名

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,35 +1,53 @@
 #!/usr/bin/env bash
 
 easy_path=/Users/${USER}/easy_data
-# easy_path=/home/${USER}/easy_data
 
+IMAGE_NAME=easy_runtime
+DOCKER_CMD=docker
 
 function main() {
    USER_ID=$(id -u)
-   USER=$(whoami)
-   GRP=$(id -g -n)
-   GRP_ID=$(id --group)
+   USER_NAME=$(whoami)
+   #GRP_NAME=$(id -g -n)
+   #GRP_ID=$(id --group)
 
    echo "Starting docker container..."
    echo "Current user id:" $USER_ID
    echo "Current user name:" $USER
-   echo "Current group id:" $GRP_IDP
-   echo "Current group name:"$GRP
+   #echo "Current group id:" $GRP_IDP
+   #echo "Current group name:"$GRP
 
    if [ ! -d $easy_path ]; then
       echo "easy_path not exist, create dir ${easy_path}"
       mkdir $easy_path
    fi
 
-    EASY_RUNTIME="easy_runtime_${USER}"
-    echo EASY_RUNTIME
-    docker ps -a --format "{{.Names}}" | grep "$EASY_RUNTIME" 1>/dev/null
+   RUNTIME_DOCKER="easy_runtime_${USER_NAME}"
+   docker ps -a --format "{{.Names}}" | grep "$RUNTIME_DOCKER" 1>/dev/null
+    
+    # 判断上次命令是否执行成功?表示上一次的执行结果
     if [ $? == 0 ]; then
-        echo "aaa" 
-        docker stop $APOLLO_DEV 1>/dev/null
-        docker rm -v -f $APOLLO_DEV 1>/dev/null
+        echo "${RUNTIME_DOCKER} is running, stop and remove ..."
+        docker stop $RUNTIME_DOCKER 1>/dev/null
+        docker rm -v -f $RUNTIME_DOCKER 1>/dev/null
+        echo "${RUNTIME_DOCKER} stop and remove success..."
     fi
 
+   echo "Starting docker container ${RUNTIME_DOCKER} ..."
+
+   ${DOCKER_CMD} run -it -d --privileged --name $RUNTIME_DOCKER \
+      -e DOCKER_IMG=$IMAGE_NAME \
+      $IMAGE_NAME \ /bin/bash
+
+   if [ $? -ne 0 ];then
+        echo "Failed to start docker container \"${RUNTIME_DOCKER}\" based on image: $IMAGE_NAME"
+        exit 1
+   fi
+
+   if [ "${USER}" != "root" ]; then
+      echo "Runtime is not root"
+        #docker exec $APOLLO_DEV bash -c '/apollo/scripts/docker_adduser.sh'
+   fi
    # docker run -it --shm-size=2g \
    #    -e DOCKER_USER_ID=$USER_ID \
    #    -e DOCKER_GRP="$GRP" \
