9b9f0bd835f5064fdd748c13437b88c5db7d44b1
Vitah <linw1225@gmail.com>
2020-12-03 15:54:57 +0800

    Feat：镜像创建时，拷贝创建非root用户脚本

---
+++ docker/runtime/add_user.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+addgroup --gid "$DOCKER_GRP_ID" "$DOCKER_GRP"
+adduser --disabled-password --force-badname --gecos '' "$DOCKER_USER" \
+    --uid "$DOCKER_USER_ID" --gid "$DOCKER_GRP_ID" 2>/dev/null
+usermod -aG sudo "$DOCKER_USER"
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+cp -r /etc/skel/. /home/${DOCKER_USER}
+
+# Set user files ownership to current user, such as .bashrc, .profile, etc.
+chown ${DOCKER_USER}:${DOCKER_GRP} /home/${DOCKER_USER}
+ls -ad /home/${DOCKER_USER}/.??* | xargs chown -R ${DOCKER_USER}:${DOCKER_GRP}
