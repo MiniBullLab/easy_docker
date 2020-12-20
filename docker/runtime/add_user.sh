1fee4382450277d1aa7f0a6ea739f8cc64c447b3
Vitah <linw1225@gmail.com>
2020-12-20 21:36:47 +0800

    Feat：docker镜像添加用户时打印内容

--- docker/runtime/add_user.sh
+++ docker/runtime/add_user.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Begin add group in image, id=${DOCKER_GRP_ID} name=${DOCKER_GRP}"
 addgroup --gid "$DOCKER_GRP_ID" "$DOCKER_GRP"
+echo "Add group in image success"
+
+echo "Begin add user in image, id=${DOCKER_USER} name=${DOCKER_USER_ID}"
 adduser --disabled-password --force-badname --gecos '' "$DOCKER_USER" \
     --uid "$DOCKER_USER_ID" --gid "$DOCKER_GRP_ID" 2>/dev/null
+echo "Add user in image success"
+
 usermod -aG sudo "$DOCKER_USER"
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 cp -r /etc/skel/. /home/${DOCKER_USER}
