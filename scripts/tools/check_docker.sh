6e794bb351ccfcfcfdc3433519cc4e073116423e
Vitah <linw1225@gmail.com>
2021-01-02 04:19:35 +0800

    Refactor：优化docker检测脚本

--- scripts/tools/check_docker.sh
+++ scripts/tools/check_docker.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-function main(){
-   GRP_ID=$(id -g)
-   GRP_NAME=$(id -g -n)
-   USER_ID=$(id -u)
-   USER_NAME=$(whoami)
-
-   docker --version | grep "Docker version" 2> /dev/null
+function main() {
+   # 检测docker是否安装
+   docker --version | grep "Docker version" 1>/dev/null 2>&1
+   # shellcheck disable=SC2181
    if [ $? != 0 ]; then
       echo "Docker没有安装，请先安装docker"
       return
    fi
 
    docker info | grep "ERROR"
+   # shellcheck disable=SC2181
    if [ $? == 0 ]; then
       echo "Docker need permission"
    fi
