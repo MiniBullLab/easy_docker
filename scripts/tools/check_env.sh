fca2800b1cc57df5000edbc158dd9df7e52759cb
Vitah <linw1225@gmail.com>
2021-01-02 21:14:15 +0800

    Refactor：优化环境检测的代码

--- scripts/tools/check_env.sh
+++ scripts/tools/check_env.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-function main() {
-   # 检测docker是否安装
+# 错误码
+CODE_DOCKER_NOT_INSTALL=10001
+
+function envCheckFailedAndExit() {
+   echo "Error code:$1"
+   exit 1
+}
+
+# 检测Docker是否安装
+function checkDockerInstall() {
    docker --version | grep "Docker version" 1>/dev/null 2>&1
    # shellcheck disable=SC2181
    if [ $? != 0 ]; then
-      echo "Docker没有安装，请先安装docker"
-      return
+      envCheckFailedAndExit $CODE_DOCKER_NOT_INSTALL
    fi
+}
+
+function main() {
+   checkDockerInstall
 
    docker info | grep "ERROR"
    # shellcheck disable=SC2181
