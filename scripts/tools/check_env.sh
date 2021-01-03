f89c27b8ec1765d4700aaba21f31c281b0c28a81
Vitah <linw1225@gmail.com>
2021-01-03 23:27:10 +0800

    Feat：环境监测nvidia-docker

--- scripts/tools/check_env.sh
+++ scripts/tools/check_env.sh
@@ -2,7 +2,8 @@
 
 # 错误码
 ERR_CODE_DOCKER_NOT_INSTALL=10001
-ERR_CODE_DOCKER_NOT_HAVE_PERMISSION=10002
+ERR_CODE_DOCKER_SOCKET_PERMISSION=10002
+ERR_CODE_NVIDIA_DOCKER_NOT_INSTALL=10003
 
 # 运行环境检测失败，打印错误码并且退出
 function envCheckFailedAndExit() {
@@ -19,18 +20,27 @@ function checkDockerInstall() {
    fi
 }
 
+function checkNvidiaDocker() {
+   nvidia-docker -v >1/dev/null
+   # shellcheck disable=SC2181
+   if [ $? == 0 ]; then
+      envCheckFailedAndExit $ERR_CODE_NVIDIA_DOCKER_NOT_INSTALL
+   fi
+}
+
 # 检测docker权限
 function checkDockerPermission() {
    docker info | grep "ERROR: Got permission denied while trying to connect to the Docker daemon socket" 1>/dev/null 2>&1
    # shellcheck disable=SC2181
    if [ $? == 0 ]; then
-      envCheckFailedAndExit $ERR_CODE_DOCKER_NOT_HAVE_PERMISSION
+      envCheckFailedAndExit $ERR_CODE_DOCKER_SOCKET_PERMISSION
    fi
 }
 
 function main() {
    checkDockerInstall
    checkDockerPermission
+   checkNvidiaDocker
 }
 
 main
