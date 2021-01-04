5046c157853ad7fbf0114bfa874cee0ef611c19e
Vitah <linw1225@gmail.com>
2021-01-04 16:12:15 +0800

    Fix：docker socket检测逻辑

--- scripts/tools/check_env.sh
+++ scripts/tools/check_env.sh
@@ -31,6 +31,7 @@ function checkNvidiaDocker() {
 # 检测docker权限
 function checkDockerPermission() {
    checkResult=$(docker info --format '{{json .}}' | grep "ERROR: Got permission denied while trying to connect to the Docker daemon socket")
+   echo "$checkResult"
    # shellcheck disable=SC2181
    if [ -z "$checkResult" ]; then
       envCheckFailedAndExit $ERR_CODE_DOCKER_SOCKET_PERMISSION
