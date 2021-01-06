7313740a41af22fb5673b3b047e9db7ab8e07844
Vitah <linw1225@gmail.com>
2021-01-06 11:40:54 +0800

    Fix：容器停止脚本

--- scripts/run/docker_stop.sh
+++ scripts/run/docker_stop.sh
@@ -3,11 +3,20 @@
 IMAGE_NAME=easy_runtime
 
 function main() {
-   USER_NAME=$(whoami)
-   RUNTIME_DOCKER="easy_runtime_${USER_NAME}"
-   echo "${RUNTIME_DOCKER} is running, begin stop..."
-   docker stop $RUNTIME_DOCKER 1>/dev/null
-   echo "${RUNTIME_DOCKER} stop success..."
+   local user_name
+   user_name=$(whoami)
+   local runtime_docker_name="${IMAGE_NAME}_${user_name}"
+
+   echo "Begin stop container ${runtime_docker_name}..."
+
+   docker_ps_result=$(docker ps -f name="${runtime_docker_name}")
+   if [ -n "$docker_ps_result" ]; then
+      echo "Docker container ${runtime_docker_name} is running, begin stop..."
+      docker stop "$runtime_docker_name" 1>/dev/null
+      echo "Docker container ${RUNTIME_DOCKER} stop success."
+   else
+      echo "Docker container ${RUNTIME_DOCKER} not in running."
+   fi
 }
 
 main
