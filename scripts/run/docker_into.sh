21d3adb8053783c195e253a16f83e3bf0921f3fa
Vitah <linw1225@gmail.com>
2021-01-11 13:44:01 +0800

    Refactor：消除shell警告

--- scripts/run/docker_into.sh
+++ scripts/run/docker_into.sh
@@ -2,7 +2,7 @@
 
 xhost +local:root 1>/dev/null 2>&1
 docker exec \
-    -u $USER \
-    -it easy_runtime_$USER \
+    -u "$USER" \
+    -it "easy_runtime_$USER" \
     /bin/bash
 xhost -local:root 1>/dev/null 2>&1
