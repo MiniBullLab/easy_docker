e95c058589b181f15655b7a0dd1b169110979908
Vitah <linw1225@gmail.com>
2021-01-24 17:54:50 +0800

    Fix：修改容器进入脚本

--- scripts/run/docker_into.sh
+++ scripts/run/docker_into.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+IMAGE_NAME=easy_ai
+CONTAINER_NAME=${IMAGE_NAME}_$USER
+
 xhost +local:root 1>/dev/null 2>&1
 docker exec \
     -u "$USER" \
-    -it "easy_runtime_$USER" \
+    -it "${CONTAINER_NAME}" \
     /bin/bash
 xhost -local:root 1>/dev/null 2>&1
