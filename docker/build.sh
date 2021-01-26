c339f391d5c85bfc69d26b3b80cbfdf9930a89d4
Vitah <linw1225@gmail.com>
2021-01-26 18:56:46 +0800

    Feat：镜像编译脚本

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+AI_IMAGE=ai
+WORKSPACE_IMAGE=workspace
+
+function main() {
+   if [ "$1" = "workspace" ]; then
+      IMAGE_NAME=$AI_IMAGE
+   else
+      IMAGE_NAME=$WORKSPACE_IMAGE
+   fi
+
+   docker-compose build "$IMAGE_NAME"
+}
+
+main "$1"
