b964117fa55af02ed05fdf7a18ba73d062f165a7
Vitah <linw1225@gmail.com>
2021-02-27 21:38:43 +0800

    Feat：分层编译

---
+++ docker/layeredBuild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+IMAGE_PREFIX=easy_
+BASE_IMAGE=base
+LIB_IMAGE_NAME=lib
+WORKSPACE_IMAGE_NAME=workspace
+RUNTIME_IMAGE_NAME=runtime
+BASE_IMAGE_NAME_VERSION=0.1
+LIB_IMAGE_NAME_VERSION=0.1
+
+function main() {
+   docker-compose build "$BASE_IMAGE"
+}
+
+main
