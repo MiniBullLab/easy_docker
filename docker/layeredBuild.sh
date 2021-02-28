7328a0a3a1183377b83d573b7d8eda2b6ef1eb6f
Vitah <linw1225@gmail.com>
2021-02-28 15:22:03 +0800

    Feat：lib image

--- docker/layeredBuild.sh
+++ docker/layeredBuild.sh
@@ -2,13 +2,17 @@
 
 DOCKER_USER=vitah
 IMAGE_PREFIX=easy_
+
+BASE_IMAGE_VERSION=1.0.0
 BASE_IMAGE=base
-BASE_IMAGE_FULL_NAME=$IMAGE_PREFIX$BASE_IMAGE
-LIB_IMAGE_NAME=lib
+BASE_IMAGE_FULL=$IMAGE_PREFIX$BASE_IMAGE
+
+LIB_IMAGE_VERSION=1.0.0
+LIB_IMAGE=lib
+LIB_IMAGE_FULL=$IMAGE_PREFIX$LIB_IMAGE
+
 WORKSPACE_IMAGE_NAME=workspace
 RUNTIME_IMAGE_NAME=runtime
-BASE_IMAGE_VERSION=1.0.0
-LIB_IMAGE_VERSION=1.0.0
 
 function dockerLogin() {
    result=$(docker login)
@@ -18,8 +22,15 @@ function dockerLogin() {
 }
 
 function main() {
+   echo "Begin build base image..."
    docker-compose build "$BASE_IMAGE"
-   docker tag $BASE_IMAGE_FULL_NAME $DOCKER_USER/$BASE_IMAGE_FULL_NAME:$BASE_IMAGE_VERSION
+   docker tag $BASE_IMAGE_FULL $DOCKER_USER/$BASE_IMAGE_FULL:$BASE_IMAGE_VERSION
+   echo "Build base image success."
+
+   echo "Begin build lib image..."
+   docker-compose build $LIB_IMAGE
+   docker tag $LIB_IMAGE_FULL $DOCKER_USER/$LIB_IMAGE_FULL:$LIB_IMAGE_VERSION
+   echo "Build lib image success."
 }
 
 main
