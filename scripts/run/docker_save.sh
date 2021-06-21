e694949aa31aba203cb8a6a136e60783de95ffc4
Vitah Lin <linw1225@gmail.com>
2021-06-21 16:47:29 +0800

    Feat: docker save shell

---
+++ scripts/run/docker_save.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DOCKER_USER=vitah
+AI_IMAGE=ai_runtime
+AI_IMAGE_VERSION=1.0.0
+SAVED_FILE_NAME="$AI_IMAGE"_"$AI_IMAGE_VERSION".tar
+
+function saveRuntimeImage() {
+   docker image ls | grep "$AI_IMAGE_VERSION" | grep "$DOCKER_USER/$AI_IMAGE" 1>/dev/null 2>&1
+   # shellcheck disable=SC2181
+   if [ $? != 0 ]; then
+      echo "Image $DOCKER_USER/$AI_IMAGE:$AI_IMAGE_VERSION not exist!"
+   else
+      docker save -o $SAVED_FILE_NAME DOCKER_USER/$AI_IMAGE:$AI_IMAGE_VERSION
+   fi
+}
+
+function main() {
+   saveRuntimeImage
+}
+
+main
