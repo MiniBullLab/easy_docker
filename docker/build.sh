d77026a194a85a6ebf98ac9d68e2813762a6ac1a
Vitah Lin <linw1225@gmail.com>
2021-07-15 14:15:42 +0800

    Fix: workspace image

--- docker/build.sh
+++ docker/build.sh
@@ -12,10 +12,6 @@ BASE_IMAGE_VERSION=1.0.0
 BASE_IMAGE=base
 BASE_IMAGE_FULL="$IMAGE_PREFIX$BASE_IMAGE"
 
-LIB_IMAGE_VERSION=1.0.0
-LIB_IMAGE=workspace_lib
-LIB_IMAGE_FULL="$IMAGE_PREFIX$LIB_IMAGE"
-
 AI_LIB_IMAGE_VERSION=1.0.0
 AI_LIB_IMAGE=runtime_lib
 AI_LIB_IMAGE_FULL="$IMAGE_PREFIX$AI_LIB_IMAGE"
@@ -49,16 +45,14 @@ function buildRuntime() {
 function buildWorkspace() {
    echo "Begin build base image..."
    docker-compose build "$BASE_IMAGE"
-   docker tag $BASE_IMAGE_FULL $DOCKER_USER/$BASE_IMAGE_FULL:$BASE_IMAGE_VERSION
    echo "Build base image success."
 
    echo "Begin build lib image..."
-   docker-compose build $LIB_IMAGE
-   docker tag $LIB_IMAGE_FULL $DOCKER_USER/$LIB_IMAGE_FULL:$LIB_IMAGE_VERSION
+   docker-compose build workspace_lib
    echo "Build lib image success."
 
    echo "Begin build workspace image..."
-   docker-compose build $WORKSPACE_IMAGE
+   docker-compose build workspace
    echo "Build workspace image success."
 }
 
